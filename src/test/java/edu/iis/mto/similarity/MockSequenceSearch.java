2c9ea0c97f7bf59aa241240f6986942721bd61b5
piotr-s-nowak <72968932+piotr-s-nowak@users.noreply.github.com>
2021-03-29 13:35:39 +0200

    Search on empty sequences

---
+++ src/test/java/edu/iis/mto/similarity/MockSequenceSearch.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
+
+public class MockSequenceSearch  implements SequenceSearcher {
+
+    @Override
+    public SearchResult search(int elem, int[] sequence) {
+        return null;
+    }
+}
--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,22 @@
 package edu.iis.mto.similarity;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SequenceSearcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testOnEmptySeq() {
+        MockSequenceSearch searcher = new MockSequenceSearch();
+        SimilarityFinder indexJaccard = new SimilarityFinder(searcher);
+        int[] sequence1 = {};
+        int[] sequence2 = {};
+        assertThat(1.0d, Matchers.equalTo(indexJaccard.calculateJackardSimilarity(sequence1, sequence2)));
+
     }
 
 }
